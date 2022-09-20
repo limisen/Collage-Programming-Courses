820349fa43bc340510f86f89aec28e3be3805023
Liam.Laptop <lc222rx@student.lnu.se>
2022-09-20 17:49:37 +0200

    Sentence builder done (if no tence check is needed

--- 1DV501/lc222rx_assign2/sentence_builder.py
+++ 1DV501/lc222rx_assign2/sentence_builder.py
@@ -1,14 +1,25 @@
-# Sentence builder
+# Sentence builder, Builds a sentence using three functions that randomly
+# pick a word and then prints the sentence
+from random import randrange
 
-pronoun()
-    
 
-verb()
-    
+def pronoun():
+    pron = ["I ", "You ", "It ", "We ", "They ", "He ", "She "]
+    x = randrange(0, len(pron))
+    return(pron[x])
 
-noun()
-    
 
-sentence()
-    
-    
+def verb():
+    verb = ["see ", "touch ", "steal ", "hug ", "drive "]
+    x = randrange(0, len(verb))
+    return(verb[x])
+
+
+def noun():
+    noun = ["coin", "pen", "car", "pc", "girl"]
+    x = randrange(0, len(noun))
+    return(noun[x])
+
+
+for i in range(0, 11):
+    print(pronoun() + "will " + verb() + "a " + noun())
--- 1DV501/lc222rx_assign2/simple_strings.py
+++ 1DV501/lc222rx_assign2/simple_strings.py
@@ -1,42 +1,63 @@
 # ha
 
+import re
+
+
 def first_last(s):
     # Prints the first and last character in the string s
     if s == "":
-        s = input()
+        s = input("Please enter a string: ")
 
     frst_chr = s[0]
     lst_chr = s[len(s) - 1]
 
-    print(f"First and last in \"{s}\": {frst_chr} and {lst_chr}\ng")
+    print(f"First and last in \"{s}\": {frst_chr} and {lst_chr}\n")
 
 
 def char_types(s):
     # Prints the number of vowels and consonants in string s
     if s == "":
-        s = input()
+        s = input("Please enter a string: ")
 
-    vowel = ["A", "E", "I", "O", "U", "Y", "W"]
+    vowel = ["A", "E", "I", "O", "U", "Y", "W", 'a', 'e', 'i' 'o', 'u', 'y']
+    v = 0
     consonants = ["B", "C", "D", "F", "G", "J", "K", "L", "M", "N", "P", "Q"]
     consonants += ["S", "T", "V", "X", "Z",  "H", "R", "W", "Y"]
+    consonants += ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm']
+    consonants += ['n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+    c = 0
 
-    print(f"In that sentence, the number of vowels is {10} ", end="")
-    print(f"and the number of consonants is {2}\n")
+    for i in range(0, len(s)):
+        for n in range(0, len(vowel)):
+            if s[i] == vowel[n]:
+                v += 1
+    for i in range(0, len(s)):
+        for n in range(0, len(consonants)):
+            if s[i] == consonants[n]:
+                c += 1
+
+    print(f"In that sentence, the number of vowels is {v} ", end="")
+    print(f"and the number of consonants is {c}\n")
 
 
 def char_symbol_number(s):
     # Prints the number of characters, symbols (including spaces)
     # and numbers in string s
     if s == "":
-        s = input()
+        s = input("Please enter a string: ")
 
-    nr_chr = len(s) - 1
-    nr_sym = "ha"
-    nrs = int(s)
+    nr_chr = len(s)
+    nr_sym = []
+
+    for i in range(0, len(s)):
+        if not (s[i].isalpha()):
+            nr_sym = nr_sym.append(s[i])
+
+    nrs = re.findall('[0-9]+', s)
     print(f"In the sentence \"{s}\" the number of letters is {18},", end="")
-    print("symbols is {11} and numbers is {nrs}\n")
+    print(f" symbols is {11} and numbers is {len(nrs)}\n")
 
 
-first_last(input("Enter a string: "))
-char_types(input("Enter a string: "))
-char_symbol_number(input("Enter a string: "))
+#first_last(input("Enter a string (F,L): "))
+#char_types(input("Enter a string (Types): "))
+char_symbol_number(input("Enter a string (sym): "))
