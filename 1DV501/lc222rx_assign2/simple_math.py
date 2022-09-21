6f92a13320a6db8b94de3cdbeec2aed5ff86e5ef
Liam.Laptop <lc222rx@student.lnu.se>
2022-09-21 11:21:59 +0200

    Simple math completed

---
+++ 1DV501/lc222rx_assign2/simple_math.py
@@ -0,0 +1,70 @@
+# ha
+
+
+def inc(n):
+    # Increments n with one
+    n += 1
+    return(n)
+
+
+def inc_with(n, t):
+    # Increments n with the value of t
+    n += t
+    return(n)
+
+
+def dec(n):
+    # Decrements n with one
+    n -= 1
+    return(n)
+
+
+def dec_with(n, t):
+    # Decrements n with the value of t
+    n -= t
+    return(n)
+
+
+def greatest(n1, n2):
+    # Returns the largest of the values n1 and n2
+    if n1 > n2:
+        return(n1)
+    elif n2 > n1:
+        return(n2)
+
+
+def is_even(n):
+    # Returns True if n is even, otherwise false
+    if (n % 2) == 0:
+        return(True)
+    else:
+        return(False)
+
+
+def power(x, n):
+    # Returns x to the power of n
+    return(x**n)
+
+
+def factorial(n):
+    # Returns the factorial of n
+    for i in range(n, 0, -1):
+        n += i*i
+    return(n)
+
+
+print(inc(45))
+
+print(inc_with(45, 5))
+
+print(dec(45))
+
+print(dec_with(45, 5))
+
+print(greatest(45, 50))
+
+print(is_even(45))
+
+print(power(45, 5))
+
+print(factorial(45))
