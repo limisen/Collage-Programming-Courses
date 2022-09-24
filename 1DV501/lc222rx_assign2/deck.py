349ab325740daeeed997ffee4b3675380957ae94
Liam.Laptop <lc222rx@student.lnu.se>
2022-09-24 10:21:40 +0200

    Fixing "no shuffle" on deck.py and minor adjustment on printing prime

--- 1DV501/lc222rx_assign2/deck.py
+++ 1DV501/lc222rx_assign2/deck.py
@@ -1,10 +1,14 @@
 # A program that generates a five card hand
 # out of a randomly generated deck of cards that is also shuffled
-from random import randrange, shuffle
+from random import choice
 cards = []
 
-for i in range(0, 52):
-    nr = randrange(2, 14)
+amount = 0
+x = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+y = {0, 1, 2, 3}
+exclude_set = set()
+while amount != 52:
+    nr = choice(list(x - exclude_set))
     if nr == 11:
         nr = "Jack"
     elif nr == 12:
@@ -14,7 +18,7 @@ for i in range(0, 52):
     elif nr == 14:
         nr = "Ace"
 
-    suit = randrange(4)
+    suit = choice(list(y - exclude_set))
     if suit == 0:
         suit = " of Clubs"
     elif suit == 1:
@@ -28,8 +32,28 @@ for i in range(0, 52):
         card = str(nr) + suit
     else:
         card = nr + suit
-    cards.append(card)
-shuffle(cards)
+    if card not in cards:
+        amount += 1
+        cards.append(card)
+    else:
+        exclude_set.add(nr)
+
+new_deck = [0] * 52
+
+A = set()
+exclude_set = set()
+for i in range(52):
+    A.add(i)
+amount = 0
+while amount != 52:
+    rng = choice(list(A - exclude_set))
+    if new_deck[rng] == 0:
+        new_deck.insert(rng, cards[amount])
+        if rng < 52:
+            new_deck.pop(rng + 1)
+        amount += 1
+    else:
+        exclude_set.add(rng)
 
 print("My hand:")
 for i in range(5):
--- 1DV501/lc222rx_assign2/print_primes.py
+++ 1DV501/lc222rx_assign2/print_primes.py
@@ -3,20 +3,19 @@
 # on the next line untill the amount equalls the requested amount.
 
 n = int(input("How many primes? "))
-x = 0
-y = 0
+amount = 0
 z = 0
 number = 0
 
-while x != n:
+while amount != n:
     number += 1
     if number > 1:
-        for i in range(2, number):
-            if (number % i) == 0:
+        for div in range(2, number):
+            if (number % div) == 0:
                 break
         else:
             z += 1
-            x += 1
+            amount += 1
             if z == 11:
                 z = 1
                 print("")
